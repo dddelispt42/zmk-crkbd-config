d4610cb51974c40acb402d092105687ba8d320c3
Heiko Riemer <hriemer@infinera.com>
2023-06-27 10:26:18 +0100

    reenable corne 3rd thumb key as repeat key

--- config/generate_keymaps.sh
+++ config/generate_keymaps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +w ./*.keymap
-grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' >./corne.keymap
-grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' >./cradio.keymap
+grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RK/\&key_repeat/g' >./corne.keymap
+grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RK//g' >./cradio.keymap
 chmod -w ./*.keymap
