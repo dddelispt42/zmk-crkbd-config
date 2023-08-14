d3bd5b8adec0ad58b42e70b689846d0615390245
Heiko Riemer <hriemer@infinera.com>
2023-08-14 11:31:34 +0100

    new combos and assign to specific layers

--- config/generate_keymaps.sh
+++ config/generate_keymaps.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 chmod +w ./*.keymap
-grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RK/\&key_repeat/g' >./corne.keymap
-grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RK//g' >./cradio.keymap
+grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&kp F23/g;s/RKR/\&key_repeat/g' >./corne.keymap
+grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RKL//g;s/RKR//g' >./cradio.keymap
 chmod -w ./*.keymap
+if command -v keymap >/dev/null; then
+	keymap parse -c 10 -z cradio.keymap >../sweep_keymap.yaml
+	keymap parse -c 10 -z corne.keymap >../corne_keymap.yaml
+	keymap draw ../sweep_keymap.yaml >../sweep_keymap.ortho.svg
+	keymap draw ../corne_keymap.yaml >../corne_keymap.ortho.svg
+	trash ../sweep_keymap.yaml ../corne_keymap.yaml
+fi
