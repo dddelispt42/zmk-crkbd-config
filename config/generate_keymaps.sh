42460bfd2c62b1bbe84b0165909f2d19f0014b75
Heiko Riemer <heiko@eheiko.net>
2025-01-22 22:04:54 +0000

    add keymap drawer and try PT locale

--- config/generate_keymaps.sh
+++ config/generate_keymaps.sh
@@ -5,9 +5,9 @@ grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&none/g;s/RKR
 grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RKL//g;s/RKR//g' >./cradio.keymap
 chmod -w ./*.keymap
 if command -v keymap >/dev/null; then
-	keymap -c ./keymap-drawer.yaml parse -c 10 -z cradio.keymap >sweep_keymap.yaml
-	keymap -c ./keymap-drawer.yaml parse -c 10 -z corne.keymap >corne_keymap.yaml
-	keymap -c ./keymap-drawer.yaml draw sweep_keymap.yaml >../sweep_keymap.ortho.svg
-	keymap -c ./keymap-drawer.yaml draw corne_keymap.yaml >../corne_keymap.ortho.svg
+	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z cradio.keymap >sweep_keymap.yaml
+	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z corne.keymap >corne_keymap.yaml
+	keymap -c ../keymap_drawer.config.yaml draw sweep_keymap.yaml >../sweep_keymap.ortho.svg
+	keymap -c ../keymap_drawer.config.yaml draw corne_keymap.yaml >../corne_keymap.ortho.svg
 	trash sweep_keymap.yaml corne_keymap.yaml
 fi
