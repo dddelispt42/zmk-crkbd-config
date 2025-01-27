5c65941fca689bd4a4b296df6058f1fb3bfb2796
Heiko Riemer <heiko@eheiko.net>
2025-01-27 15:03:22 +0000

    disable morph from space to return

--- config/generate_keymaps.sh
+++ config/generate_keymaps.sh
@@ -4,10 +4,10 @@ chmod +w ./*.keymap
 grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&none/g;s/RKR/\&key_repeat/g' >./corne.keymap
 grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RKL//g;s/RKR//g' >./cradio.keymap
 chmod -w ./*.keymap
-if command -v keymap >/dev/null; then
-	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z cradio.keymap >sweep_keymap.yaml
-	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z corne.keymap >corne_keymap.yaml
-	keymap -c ../keymap_drawer.config.yaml draw sweep_keymap.yaml >../sweep_keymap.ortho.svg
-	keymap -c ../keymap_drawer.config.yaml draw corne_keymap.yaml >../corne_keymap.ortho.svg
-	trash sweep_keymap.yaml corne_keymap.yaml
-fi
+# if command -v keymap >/dev/null; then
+# 	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z cradio.keymap >sweep_keymap.yaml
+# 	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z corne.keymap >corne_keymap.yaml
+# 	keymap -c ../keymap_drawer.config.yaml draw sweep_keymap.yaml >../sweep_keymap.ortho.svg
+# 	keymap -c ../keymap_drawer.config.yaml draw corne_keymap.yaml >../corne_keymap.ortho.svg
+# 	trash sweep_keymap.yaml corne_keymap.yaml
+# fi
