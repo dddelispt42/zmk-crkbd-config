5493efdfe7d5419fe639d65f46a76f0b90e7a05a
Heiko Riemer <heiko@eheiko.net>
2025-06-15 22:04:47 +0100

    reset sweep/cradio to zmk-helper

--- config/generate_keymaps.sh
+++ config/generate_keymaps.sh
@@ -2,7 +2,7 @@
 chmod +w ./*.keymap
 # grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&leader1/g;s/RKR/\&key_repeat/g' >./corne.keymap
 grep -v "NOT-FOR-CORNE" keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&none/g;s/RKR/\&key_repeat/g' | sed 's/\s*$//' >./corne.keymap
-grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RKL//g;s/RKR//g' | sed 's/\s*$//' >./cradio.keymap
+# grep -v "NOT-FOR-SWEEP" keymap | sed 's/DK//g' | sed 's/RKL//g;s/RKR//g' | sed 's/\s*$//' >./cradio.keymap
 chmod -w ./*.keymap
 if command -v keymap >/dev/null; then
 	keymap -c ../keymap_drawer.config.yaml parse -c 10 -z cradio.keymap >sweep_keymap.yaml
