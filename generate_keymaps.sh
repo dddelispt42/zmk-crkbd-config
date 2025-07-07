110dedcc2ee096c5b03b9a366eeff4ccdedb5bea
Heiko Riemer <heiko@eheiko.net>
2025-07-07 16:06:52 +0100

    first personal keymap for tempest

--- generate_keymaps.sh
+++ generate_keymaps.sh
@@ -3,7 +3,7 @@ chmod +w ./config/*.keymap
 # grep -v "NOT-FOR-CORNE" ./config/keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&leader1/g;s/RKR/\&key_repeat/g' >./config/corne.keymap
 grep -vE "ONLY-FOR-SWEEP|ONLY-FOR-TEMPEST" ./config/keymap | sed 's/DK/\&none/g' | sed 's/RKL/\&none/g;s/RKR/\&key_repeat/g' | sed 's/\s*$//' >./config/corne.keymap
 # grep -vE "ONLY-FOR-CORNE|ONLY-FOR-TEMPEST" ./config/keymap | sed 's/DK//g' | sed 's/RKL//g;s/RKR//g' | sed 's/\s*$//' >./config/cradio.keymap
-grep -vE "ONLY-FOR-CORNE|ONLY-FOR-SWEEP" ./config/keymap | sed 's/DK//g' | sed 's/RKL/&none/g;s/RKR/\&key_repeat/g' | sed 's/\s*$//' >./config/tempest.keymap
+grep -vE "ONLY-FOR-CORNE|ONLY-FOR-SWEEP" ./config/keymap | sed 's/DK//g' | sed 's/RKL/\&none/g;s/RKR/\&key_repeat/g' | sed 's/\s*$//' >./config/tempest.keymap
 chmod -w ./config/*.keymap
 if command -v keymap >/dev/null; then
 	keymap -c keymap_drawer.config.yaml parse -c 10 -z ./config/corne.keymap >corne_keymap.yaml
